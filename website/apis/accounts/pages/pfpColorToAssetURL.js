a98bc1ad35043642fadf92549de506068e67fcdf
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2022-09-04 11:45:07 -0600

    make pfpColorToAssetURL actually good

--- website/apis/accounts/pages/pfpColorToAssetURL.js
+++ website/apis/accounts/pages/pfpColorToAssetURL.js
@@ -5,6 +5,6 @@ module.exports.documentation = {
     "explanation": `Turns a PFP color into a URL to an image on the server. Use the <code>color</code> query parameter to specify the profile color.`
 }
 module.exports.onPage = async (req, res, origin, isAnS4DUrl, bypassedS4DOriginCheck, originIncorrectFunction) => {
-    res.status(200)
-    res.json({ url: `https://s4d469apis.scratch4discord.repl.co/fileAssets/profiles/${req.query.color}.png` })
+    res.status(accounts.allProfileColors.includes(req.query.color) ? 200 : 400)
+    res.json({ url: (accounts.allProfileColors.includes(req.query.color) ? `https://s4d469apis.scratch4discord.repl.co/fileAssets/profiles/${req.query.color}.png` : null) })
 }
