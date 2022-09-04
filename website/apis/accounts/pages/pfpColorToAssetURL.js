3a7f0e0d9fb8663b3fc24028041a70e74632306b
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2022-09-04 11:47:12 -0600

    34rtg

--- website/apis/accounts/pages/pfpColorToAssetURL.js
+++ website/apis/accounts/pages/pfpColorToAssetURL.js
@@ -2,7 +2,8 @@ const accounts = require("../module.js")
 module.exports.pageType = "get"
 module.exports.documentation = {
     "documented": true,
-    "explanation": `Turns a PFP color into a URL to an image on the server. Use the <code>color</code> query parameter to specify the profile color.`
+    "explanation": `Turns a PFP color into a URL to an image on the server. Use the <code>color</code> query parameter to specify the profile color.
+An object containing a key named <code>url</code> will be sent back. The value of this will be null if the color is invalid.`
 }
 module.exports.onPage = async (req, res, origin, isAnS4DUrl, bypassedS4DOriginCheck, originIncorrectFunction) => {
     res.status(accounts.allProfileColors.includes(req.query.color) ? 200 : 400)
